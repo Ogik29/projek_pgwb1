fcc97c63f0216407b0134258d20f53b4e34fa42e
Dimas Ogi <dimasoki1234@gmail.com>
2022-10-25 14:02:30 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,90 +20,90 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
-        // Siswa::create([
-        //     'nisn' => 1232234,
-        //     'nama' => 'Makima',
-        //     'alamat' => 'Chainsaw',
-        //     'jenis_kelamin' => 'Wanita',
-        //     'email' => 'makima@gmail.com',
-        //     'foto' => 'makima.png',
-        //     'about' => 'Hot'
-        // ]);
+        Siswa::create([
+            'nisn' => 1232234,
+            'nama' => 'Makima',
+            'alamat' => 'Chainsaw',
+            'jenis_kelamin' => 'Wanita',
+            'email' => 'makima@gmail.com',
+            'foto' => 'makima.png',
+            'about' => 'Hot'
+        ]);
 
-        // Siswa::create([
-        //     'nisn' => 1232443,
-        //     'nama' => 'Quanxi',
-        //     'alamat' => 'Chainsaw',
-        //     'jenis_kelamin' => 'Wanita',
-        //     'email' => 'quanxi@gmail.com',
-        //     'foto' => 'quanxi.png',
-        //     'about' => 'Badass'
-        // ]);
+        Siswa::create([
+            'nisn' => 1232443,
+            'nama' => 'Quanxi',
+            'alamat' => 'Chainsaw',
+            'jenis_kelamin' => 'Wanita',
+            'email' => 'quanxi@gmail.com',
+            'foto' => 'quanxi.png',
+            'about' => 'Badass'
+        ]);
 
-        // Siswa::create([
-        //     'nisn' => 1232989,
-        //     'nama' => 'Power',
-        //     'alamat' => 'Chainsaw',
-        //     'jenis_kelamin' => 'Wanita',
-        //     'email' => 'power@gmail.com',
-        //     'foto' => 'power.png',
-        //     'about' => 'Funny'
-        // ]);
+        Siswa::create([
+            'nisn' => 1232989,
+            'nama' => 'Power',
+            'alamat' => 'Chainsaw',
+            'jenis_kelamin' => 'Wanita',
+            'email' => 'power@gmail.com',
+            'foto' => 'power.png',
+            'about' => 'Funny'
+        ]);
 
-        // JenisKontak::create([
-        //     'jenis_kontak' => 'Instagram'
-        // ]);
+        JenisKontak::create([
+            'jenis_kontak' => 'Instagram'
+        ]);
 
-        // JenisKontak::create([
-        //     'jenis_kontak' => 'WhatsApp'
-        // ]);
+        JenisKontak::create([
+            'jenis_kontak' => 'WhatsApp'
+        ]);
 
-        // JenisKontak::create([
-        //     'jenis_kontak' => 'Discord'
-        // ]);
+        JenisKontak::create([
+            'jenis_kontak' => 'Discord'
+        ]);
 
-        // Jenis_Kontak_siswa::create([
-        //     'siswa_id' => 1,
-        //     'jenis_kontak_id' => 2,
-        //     'deskripsi' => '0844476899'
-        // ]);
+        Jenis_Kontak_siswa::create([
+            'siswa_id' => 1,
+            'jenis_kontak_id' => 2,
+            'deskripsi' => '0844476899'
+        ]);
 
-        // Projek::create([
-        //     'siswa_id' => 1,
-        //     'nama_projek' => 'Project 1',
-        //     'deskripsi' => 'First Project cuy',
-        //     'tanggal' => now()
-        // ]);
+        Projek::create([
+            'siswa_id' => 1,
+            'nama_projek' => 'Project 1',
+            'deskripsi' => 'First Project cuy',
+            'tanggal' => now()
+        ]);
 
-        // Projek::create([
-        //     'siswa_id' => 1,
-        //     'nama_projek' => 'Project 2',
-        //     'deskripsi' => 'Second Project cuy',
-        //     'tanggal' => now()
-        // ]);
+        Projek::create([
+            'siswa_id' => 1,
+            'nama_projek' => 'Project 2',
+            'deskripsi' => 'Second Project cuy',
+            'tanggal' => now()
+        ]);
 
-        // Projek::create([
-        //     'siswa_id' => 2,
-        //     'nama_projek' => 'Project Quanxi',
-        //     'deskripsi' => 'Quanxi Project',
-        //     'tanggal' => now()
-        // ]);
+        Projek::create([
+            'siswa_id' => 2,
+            'nama_projek' => 'Project Quanxi',
+            'deskripsi' => 'Quanxi Project',
+            'tanggal' => now()
+        ]);
 
-        // Projek::create([
-        //     'siswa_id' => 3,
-        //     'nama_projek' => 'Power',
-        //     'deskripsi' => 'Power Project',
-        //     'tanggal' => now()
-        // ]);
+        Projek::create([
+            'siswa_id' => 3,
+            'nama_projek' => 'Power',
+            'deskripsi' => 'Power Project',
+            'tanggal' => now()
+        ]);
 
-        // Projek::factory(10)->create();
+        Projek::factory(10)->create();
 
         User::create([
             'name' => 'Lemao',
